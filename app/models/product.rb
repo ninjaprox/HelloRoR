a5d5325f07fba516f6e92e6143f280f10204b27e
Nguyen Vinh <ninjaprox@gmail.com>
2014-07-29 17:38:07 +0700

    Initial manage product

---
+++ app/models/product.rb
@@ -0,0 +1,4 @@
+class Product < ActiveRecord::Base
+	validates :name, presence: true, length: { maximum: 50 }
+	validates :price, presence: true, numericality: { greater_than: 0.0 }
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140729102415) do
+ActiveRecord::Schema.define(version: 20140729103202) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "products", force: true do |t|
+    t.string   "name"
+    t.float    "price"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
