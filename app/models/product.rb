06a9ca24c710837c20764845dbd24f0be5565aa0
Nguyen Vinh <ninjaprox@gmail.com>
2014-07-30 14:48:23 +0700

    Add association between Category and Product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ActiveRecord::Base
+	belongs_to :category
+
 	validates :name, presence: true, length: { maximum: 50 }
 	validates :price, presence: true, numericality: { greater_than: 0.0 }
 end
