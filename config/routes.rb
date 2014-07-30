b290b6cc9df115789ff453816c0d992075ec3d0b
Nguyen Vinh <ninjaprox@gmail.com>
2014-07-30 14:47:20 +0700

    Add static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   resources :users
-
   resources :products
-
   resources :categories
 
+  root 'static_pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
