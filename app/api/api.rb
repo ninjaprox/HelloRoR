0deb98bb1e827087ac5b63d674aa7b0f4e035adb
Nguyen Vinh <ninjaprox@gmail.com>
2014-07-31 01:27:11 +0700

    Expose API

---
+++ app/api/api.rb
@@ -0,0 +1,21 @@
+class API < Grape::API
+  prefix 'api'
+  format :json
+
+  resource :products do
+    get do
+      Product.select('id, name, price, category_id')
+    end
+  end
+
+  resource :users do
+    params do
+      requires :name, type: String
+      requires :link, type: String
+    end
+
+    post do
+      User.create(name: params[:name], link: params[:link])
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   resources :categories
 
   root 'static_pages#home'
+
+  mount API => '/'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
